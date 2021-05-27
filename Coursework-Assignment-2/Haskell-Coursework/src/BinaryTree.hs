2ef14bb4ba37982c606d01cfac8a314a4ec7c6f5
Stephen-Anderson-2000 <steppyandrews@gmail.com>
2021-05-27 17:38:56 +0100

    Passes insertion tests and added worker functions to assist in visually checking order

--- Coursework-Assignment-2/Haskell-Coursework/src/BinaryTree.hs
+++ Coursework-Assignment-2/Haskell-Coursework/src/BinaryTree.hs
@@ -1,10 +1,14 @@
 module BinaryTree (
     BST,
     newEmptyBST,
+    testTree,
     newBSTFromItem,
     newBSTFromList,
     addItem,
-    lookupBST
+    addItems,
+    lookupBST,
+    bstToList,
+    displayBST
     ) where
 
 data BST item = Leaf
@@ -16,22 +20,22 @@ testTree :: BST String
 testTree =
     Node 22 "Mary"
         (Node 0 "Harry"
-            (Node 4 "Steve" 
-                (Node (-1) "Ed" Leaf Leaf)  
-                (Node 1 "Will" Leaf Leaf)  
-            )  
-            (Node 9 "Ed"  
-                Leaf  
-                (Node 19 "Henry" Leaf Leaf)  
-            )  
-        )  
-        (Node 37 "Vicky"  
-            (Node 26 "Charlie"  
-                (Node 24 "Jim" 
-                    (Node 23 "Liz" Leaf Leaf) 
+            (Node 4 "Steve"
+                (Node (-1) "Ed" Leaf Leaf)
+                (Node 1 "Will" Leaf Leaf)
+            )
+            (Node 9 "Ed"
+                Leaf
+                (Node 19 "Henry" Leaf Leaf)
+            )
+        )
+        (Node 37 "Vicky"
+            (Node 26 "Charlie"
+                (Node 24 "Jim"
+                    (Node 23 "Liz" Leaf Leaf)
                     Leaf
-                )  
-                (Node 31 "Anne" Leaf Leaf)  
+                )
+                (Node 31 "Anne" Leaf Leaf)
             )
             (Node 42 "John" Leaf Leaf)
         )
@@ -45,9 +49,8 @@ newBSTFromItem :: Int -> item -> BST item
 newBSTFromItem key value = Node key value Leaf Leaf
 
 
-newBSTFromList :: [(Int, item)] -> BST item
-newBSTFromList list = newEmptyBST
--- currently not implemented as it needs to use addItem?
+newBSTFromList :: Eq item => [(Int, item)] -> BST item
+newBSTFromList list = addItems list newEmptyBST
 
 
 addItem :: Int -> item -> BST item -> BST item
@@ -58,10 +61,23 @@ addItem key value tree@(Node rootKey rootItem leftChild rightChild)
     | key > rootKey =
         addItem key value rightChild
     | key == rootKey =
+        -- Overwrites existing value if keys match
         Node key value leftChild rightChild
     | otherwise =
         tree
-        
+
+addItems :: Eq item => [(Int, item)] -> BST item -> BST item
+addItems items Leaf = do
+    if items /= [] then
+        let root = head items in
+            addItems (tail items) (addItem (fst root) (snd root) Leaf)
+    else Leaf
+addItems items tree@(Node rootKey rootItem leftChild rightChild) = do
+    if items /= [] then
+        let root = head items in
+            addItems (tail items) (addItem (fst root) (snd root) Leaf)
+    else tree
+
 
 lookupBST :: Int -> BST item -> Maybe item
 lookupBST soughtKey Leaf = Nothing
@@ -72,3 +88,24 @@ lookupBST soughtKey (Node key item leftChild rightChild)
     lookupBST soughtKey rightChild
   | otherwise =
     Just item
+
+
+nodeToString :: Show item => BST item -> String
+nodeToString node@(Node key item leftChild rightChild) =
+    show key ++ ", " ++ show item
+
+
+bstToList :: Eq item => BST item -> [(Int, item)] -> [(Int, item)]
+bstToList tree@(Node key item leftChild rightChild) list =
+    list ++ bstToList leftChild list
+    ++ [(key, item)]
+    ++ bstToList rightChild list
+bstToList Leaf list = list
+
+
+displayBST :: Show item => BST item -> IO ()
+displayBST Leaf = return ()
+displayBST tree@(Node key item leftChild rightChild) = do
+    displayBST leftChild
+    putStrLn (nodeToString tree)
+    displayBST rightChild
